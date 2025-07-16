fdc236857cf5e8c2f269c491b67e1e2156ddda4b
Alex Degtyarev <degtyarevaleksandr2005@gmail.com>
2025-07-16 22:11:18 +0300

    first realisation

---
+++ build/test/CMakeFiles/pgw_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pgw_tests.dir/data_plane_test.cpp.o"
+  "CMakeFiles/pgw_tests.dir/data_plane_test.cpp.o.d"
+  "pgw_tests"
+  "pgw_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pgw_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/test/CMakeFiles/simple_pgw_tests.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_pgw_tests.dir/build/CMakeFiles/3.28.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.o"
+  "CMakeFiles/simple_pgw_tests.dir/build/CMakeFiles/3.28.3/CompilerIdCXX/CMakeCXXCompilerId.cpp.o.d"
+  "CMakeFiles/simple_pgw_tests.dir/data_plane_test.cpp.o"
+  "CMakeFiles/simple_pgw_tests.dir/data_plane_test.cpp.o.d"
+  "simple_pgw_tests"
+  "simple_pgw_tests.pdb"
+  "simple_pgw_tests[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_pgw_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
